b5e53ef8d0af4752df0cf2cf07fcd4246764ee53
b3nharris <benharris.wash@outlook.com>
2023-11-27 22:13:48 +0000

    Reached issue with missing data for poverty in many years

--- WB_data.R
+++ WB_data.R
@@ -2,28 +2,33 @@ library(tidyverse)
 library(wbstats)
 #import WB datasets-----
 ##GDP----
-GDP <- wb_data(indicator = "SI.POV.DDAY",
+povertyin <- wb_data(indicator = "SI.POV.DDAY",
                country = "countries_only",
                start_date = 2000,
                end_date = 2022)
 
 ##access to basic water----
-WATER <- wb_data(indicator = "SH.H2O.BASW.ZS",
+basicwaterin <- wb_data(indicator = "SH.H2O.BASW.ZS",
                  country = "countries_only",
                  start_date = 2000,
                  end_date = 2022)
 
 ##country list for income status----
-countries <- wb_countries()
+countriesin <- wb_countries()
 
-#filter GDP and water for only LIC and only 2000 & 2022
-GDP_LIC <- GDP |> left_join(countries |> 
+#filter GDP and water for only LIC and only 2000 & 2022-----
+poverty_lic <- povertyin |> left_join(countriesin |> 
                               select(iso3c, income_level_iso3c), join_by(iso3c)) |> 
   filter(income_level_iso3c =="LIC",
          date %in% c(2000,2022))
 
 
-WATER_LIC <- WATER |> left_join(countries |> 
+water_lic <- basicwaterin |> left_join(countriesin |> 
                                          select(iso3c, income_level_iso3c), join_by(iso3c)) |> 
   filter(income_level_iso3c =="LIC",
          date %in% c(2000,2022))
+
+#join tables for plotting-----
+poverty_water <- poverty_lic |> left_join(water_lic  |> 
+                                        select(iso3c, date, SH.H2O.BASW.ZS), join_by(iso3c, date))
+#This fails because countries don't have poverty data for each year - need to impute values?
