b82812d78601dfa5de25e110c3402f6edcd26cb7
b3nharris <benharris.wash@outlook.com>
2023-11-27 22:59:00 +0000

    need to mutate access to water befor eplotting

--- WB_data.R
+++ WB_data.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(wbstats)
+library(imputeTS)
+library(janitor)
 #import WB datasets-----
 ##GDP----
 povertyin <- wb_data(indicator = "SI.POV.DDAY",
@@ -16,8 +18,16 @@ basicwaterin <- wb_data(indicator = "SH.H2O.BASW.ZS",
 ##country list for income status----
 countriesin <- wb_countries()
 
+#interpolate TS data for poverty-----
+poverty_complete <- povertyin |> 
+  na_kalman()
+
+#calculate no access to water -----
+water_no <- basicwaterin |> 
+  mutate()
+
 #filter GDP and water for only LIC and only 2000 & 2022-----
-poverty_lic <- povertyin |> left_join(countriesin |> 
+poverty_lic <- poverty_complete |> left_join(countriesin |> 
                               select(iso3c, income_level_iso3c), join_by(iso3c)) |> 
   filter(income_level_iso3c =="LIC",
          date %in% c(2000,2022))
@@ -30,5 +40,13 @@ water_lic <- basicwaterin |> left_join(countriesin |>
 
 #join tables for plotting-----
 poverty_water <- poverty_lic |> left_join(water_lic  |> 
-                                        select(iso3c, date, SH.H2O.BASW.ZS), join_by(iso3c, date))
-#This fails because countries don't have poverty data for each year - need to impute values?
+                                        select(iso3c, date, SH.H2O.BASW.ZS), join_by(iso3c, date)) |> 
+  janitor::clean_names()
+  
+#plot!-----
+ggplot(data = poverty_water, mapping = aes(x = date,
+                                           y = sh_h2o_basw_zs)) +
+         geom_line()+
+        facet_wrap(~country)
+        facet
+       
